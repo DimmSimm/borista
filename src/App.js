94fdc46384e6ca3bafd262280d3569cf82757a00
DimmSimm <xdxp@mail.ru>
2024-09-29 14:03:24 +0500

    borista

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import About from './components/about/About';
+import Services from './components/services/Services';
+import Career from './components/career/Career';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <About />
+        <Services />
+        <Career />
       </header>
     </div>
   );
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,53 +1,34 @@
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 // import BtnDarkMode from '../btnDarkMode/BtnDarkMode';
 import './style.css';
 
+
 const Navbar = () => {
-	const activeLink = 'nav-list__link nav-list__link--active';
-	const normalLink = 'nav-list__link';
+	// const activeLink = 'nav-list__link nav-list__link--active';
+	// const normalLink = 'nav-list__link';
 
 	return (
 		<nav className="nav">
 			<div className="container">
 				<div className="nav-row">
-					<NavLink to="/" className="logo">
-						<strong>Freelancer</strong> portfolio
-					</NavLink>
-
-					<BtnDarkMode />
+					<div className="logo">
+						<strong>ЭТОСОФТ</strong>
+					</div>
 
 					<ul className="nav-list">
 						<li className="nav-list__item">
-							<NavLink
-								to="/"
-								className={({ isActive }) =>
-									isActive ? activeLink : normalLink
-								}
-							>
-								Home
-							</NavLink>
+							О компании
 						</li>
 
 						<li className="nav-list__item">
-							<NavLink
-								to="/projects"
-								className={({ isActive }) =>
-									isActive ? activeLink : normalLink
-								}
-							>
-								Projects
-							</NavLink>
+							Услуги и решения
 						</li>
 						<li className="nav-list__item">
-							<NavLink
-								to="/contacts"
-								className={({ isActive }) =>
-									isActive ? activeLink : normalLink
-								}
-							>
-								Contacts
-							</NavLink>
+							Карьера
 						</li>
+                        <li>
+                            +7 (908) 058-67-55
+                        </li>
 					</ul>
 				</div>
 			</div>
